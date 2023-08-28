8f7970a7d1d7dc2a6221f8741b4992cba8f05aa9
MiiTHeRsZ <gustavo_leme05@hotmail.com>
2023-08-28 15:45:34 -0300

    declarations update

--- PlayPieceAPI/src/main/java/com/playpiece/PlayPiece/Controllers/PessoaController.java
+++ PlayPieceAPI/src/main/java/com/playpiece/PlayPiece/Controllers/PessoaController.java
@@ -10,14 +10,12 @@ import com.playpiece.PlayPiece.Services.*;
 import java.util.*;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -78,6 +76,11 @@ public class PessoaController {
     @DeleteMapping("/{id}")
     public ResponseEntity<PessoaModel> statusPessoa(@PathVariable int id){
         return new ResponseEntity<>(pessoaService.statusPessoa(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PessoaModel> statusPessoa(@PathVariable int id) {
+        return new ResponseEntity<>(usuarioService.statusPessoa(id), HttpStatus.OK);
     }
 
 }
--- PlayPieceAPI/src/main/java/com/playpiece/PlayPiece/repositories/ContatoRepository.java
+++ PlayPieceAPI/src/main/java/com/playpiece/PlayPiece/repositories/ContatoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.playpiece.PlayPiece.Models.ContatoModel;
 import org.springframework.stereotype.Repository;
 
-    @Repository
-    public interface ContatoRepository extends JpaRepository<ContatoModel, Integer> {
-    }
+@Repository
+public interface ContatoRepository extends JpaRepository<ContatoModel, Integer> {
+}
