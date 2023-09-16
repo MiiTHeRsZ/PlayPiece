4873e0bac63a9bd82b2330ac4bf2e2c652c2872b
iansaggior <ian.saggior.pereira@gmail.com>
2023-09-16 16:22:59 -0300

    type_columns_dto

--- PlayPieceAPI/src/main/java/com/playpiece/PlayPiece/Models/ProdutoModel.java
+++ PlayPieceAPI/src/main/java/com/playpiece/PlayPiece/Models/ProdutoModel.java
@@ -19,13 +19,13 @@ public class ProdutoModel {
     private Long id;
 
     private String nome;
-
+    @Column(name = "nome", columnDefinition = "VARCHAR(40)")
     private double avaliacao;
-
+    @Column(name = "descricao", columnDefinition = "LONGTEXT") 
     private String descricao;
-
+    @Column(name = "preco", columnDefinition = "DECIMAL(10,2)")
     private double preco;
-
+    @Column(name = "quantidade", columnDefinition = "INT")
     private int quantidade;
 
     @Transient
