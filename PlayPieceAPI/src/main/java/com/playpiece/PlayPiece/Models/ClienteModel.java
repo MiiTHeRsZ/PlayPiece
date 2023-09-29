3c84552472676f4ed0a5fbe2592cd4ec18eb70a0
Leonardo Fujimura <leonardo.fujimura123@gmail.com>
2023-09-29 01:19:45 -0300

    adição Cliente e Endereço

---
+++ PlayPieceAPI/src/main/java/com/playpiece/PlayPiece/Models/ClienteModel.java
@@ -0,0 +1,32 @@
+package com.playpiece.PlayPiece.Models;
+
+import java.sql.Date;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "cliente")
+@Entity(name = "cliente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ClienteModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CPF
+    private String cpf;
+    private String nome;
+    private Date dt_nascimento;
+    private String genero;
+    private String email;
+    private String senha;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "end_fat", referencedColumnName = "id")
+    private String enderecoFaturamento;
+    private Boolean ativo;
+}
