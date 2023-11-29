5190e226bae90e9ad8284ac960da0d81e08d3796
Leonardo Fujimura <leonardo.fujimura123@gmail.com>
2023-11-29 17:29:44 -0300

    change db

--- DataBase/criacaoDB.sql
+++ DataBase/criacaoDB.sql
@@ -56,7 +56,7 @@ create table cliente(
 drop table if exists endereco;
 create table endereco(
 	endereco_id bigint primary key auto_increment,
-	fk_cliente_id bigint not null,
+	fk_cliente_id bigint,
 	cep varchar(8) not null,
 	logradouro varchar(255) not null,
 	numero int not null,
