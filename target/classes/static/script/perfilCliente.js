387f68ffea561adcf98495fc15361f54176ddda2
Henrick Adrian <henrick.adrian4@gmail.com>
2023-12-04 14:21:54 -0300

    Gusta improves
    Co-Authored-By: Gustavo Leme <103009063+MiiTHeRsZ@users.noreply.github.com>

---
+++ target/classes/static/script/perfilCliente.js
@@ -0,0 +1,220 @@
+function getCookie(nome) {
+    return Cookies.get(nome)
+}
+function setCookie(nome, info, exdays) {
+    Cookies.set(nome, info, exdays)
+}
+
+function checkCookie(nome) {
+    var sessaoId = getCookie(nome);
+    if (sessaoId == undefined) {
+        alert("Usuário deslogado, redirecionando para login")
+        location.href = "./loginCliente.html"
+    }
+}
+checkCookie('sessaoId')
+
+idCliente = getCookie('sessaoId')
+
+function menu() {
+    let carrinho = sessionStorage.getItem('carrinho');
+
+    if (carrinho != "" && carrinho != null && carrinho != undefined) {
+        let cont = 0;
+
+        if (carrinho.length > 3) {
+            carrinho.split(",").forEach(item => {
+                cont++;
+            });
+        } else {
+            cont++;
+        }
+
+        document.getElementById("notificacaoCarrinho").innerHTML = cont;
+        document.getElementById("notificacaoCarrinho").style.display = "inline";
+    }
+}
+menu();
+
+document.getElementById("sair-perfil").addEventListener("click", (e) => {
+    e.preventDefault();
+    const resp = window.confirm("Deseja encerrar a sessão?");
+    if (resp == 1) {
+        Cookies.remove('sessaoId');
+        Cookies.remove('nome');
+        sessionStorage.removeItem("carrinho");
+        window.open("../index.html", "_self");
+    }
+});
+
+let dados;
+
+const conectAPI = async () => {
+    dados = await fetch(`/cliente/${idCliente}`).then(data => data.json());
+    preecheDados();
+}
+conectAPI();
+
+const preecheDados = async () => {
+    document.getElementById("nomeCliente").innerHTML = `Olá, ${dados.nome}!`;
+    document.getElementById("idCliente").innerHTML = dados.clienteId;
+
+    document.getElementById("cpf").value = dados.cpf;
+    document.getElementById("nome").value = dados.nome;
+    document.getElementById("dt_nasc").value = dados.dt_nascimento;
+    document.getElementById("genero").value = dados.genero;
+    document.getElementById("email").value = dados.email;
+
+    document.getElementById("cepFat").value = dados.enderecoFaturamento.cep;
+    document.getElementById("logradouroFat").value = dados.enderecoFaturamento.logradouro;
+    document.getElementById("numeroFat").value = dados.enderecoFaturamento.numero;
+    document.getElementById("complementoFat").value = dados.enderecoFaturamento.complemento;
+    document.getElementById("bairroFat").value = dados.enderecoFaturamento.bairro;
+    document.getElementById("cidadeFat").value = dados.enderecoFaturamento.cidade;
+    document.getElementById("ufFat").value = dados.enderecoFaturamento.uf;
+
+    dados.listaEndereco.forEach(endereco => {
+        let opcao = document.createElement("option");
+        opcao.value = endereco.enderecoId;
+        if (endereco.padrao) {
+            opcao.toggleAttribute("selected");
+            document.getElementById("cep").value = endereco.cep;
+            document.getElementById("logradouro").value = endereco.logradouro;
+            document.getElementById("numero").value = endereco.numero;
+            document.getElementById("complemento").value = endereco.complemento;
+            document.getElementById("bairro").value = endereco.bairro;
+            document.getElementById("cidade").value = endereco.cidade;
+            document.getElementById("uf").value = endereco.uf;
+            document.getElementById("definir-endereco-padrao").toggleAttribute("disabled");
+            document.getElementById("excluir-endereco").toggleAttribute("disabled");
+        }
+        if (!endereco.ativo) {
+            opcao.toggleAttribute("disabled");
+        }
+        opcao.textContent = endereco.logradouro + ", N°" + endereco.numero;
+        if (endereco.padrao) {
+            opcao.textContent += ` \u2B50`
+        }
+        document.getElementById("enderecoEntrega").appendChild(opcao);
+    });
+}
+
+document.getElementById("enderecoEntrega").addEventListener("change", () => {
+
+    dados.listaEndereco.forEach(endereco => {
+
+        if (document.getElementById("enderecoEntrega").value == endereco.enderecoId) {
+            document.getElementById("cep").value = endereco.cep;
+            document.getElementById("logradouro").value = endereco.logradouro;
+            document.getElementById("numero").value = endereco.numero;
+            document.getElementById("complemento").value = endereco.complemento;
+            document.getElementById("bairro").value = endereco.bairro;
+            document.getElementById("cidade").value = endereco.cidade;
+            document.getElementById("uf").value = endereco.uf;
+
+            if (endereco.padrao) {
+                document.getElementById("definir-endereco-padrao").toggleAttribute("disabled");
+                document.getElementById("excluir-endereco").toggleAttribute("disabled");
+            } else {
+                document.getElementById("definir-endereco-padrao").removeAttribute("disabled");
+                document.getElementById("excluir-endereco").removeAttribute("disabled");
+            }
+        }
+
+    });
+
+});
+
+document.getElementById("alterar-info").addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    if (document.getElementById("senha").value === document.getElementById("confirmaSenha").value) {
+
+        let cliente = {
+            "cpf": document.getElementById("cpf").value,
+            "nome": document.getElementById("nome").value,
+            "dt_nascimento": document.getElementById("dt_nasc").value,
+            "genero": document.getElementById("genero").value,
+            "email": document.getElementById("email").value,
+            "enderecoFaturamento": dados.enderecoFaturamento,
+            "listaEndereco": dados.listaEndereco,
+            "senha": document.getElementById("senha").value.length == 0 ? dados.senha : document.getElementById("senha").value,
+            "ativo": true
+        }
+
+        const result = await fetch(`/cliente/${document.getElementById("idCliente").innerHTML}`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(cliente),
+        })
+
+        if (result.status == 200 || result.status == 201) {
+            alert("Dados atualizados com sucesso!");
+            window.location.reload();
+        } else {
+            alert("Falha ao atualizar os dados\nTente novamente!");
+        }
+    } else {
+        alert("Senhas não coincidem!\nCaso não deseje alterar a senha,\ngaranta que os campos de 'senha' estejam vazios!");
+    }
+});
+
+document.getElementById("definir-endereco-padrao").addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const result = await fetch(`/endereco/${document.getElementById("enderecoEntrega").value}`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: '',
+    })
+
+    if (result.status == 200 || result.status == 201) {
+        alert("Endereço padrão definido com sucesso!");
+        window.location.reload();
+    } else {
+        alert("Falha ao definir o endereço padrão\nTente novamente!");
+    }
+});
+
+document.getElementById("excluir-endereco").addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const result = await fetch(`/endereco/${document.getElementById("enderecoEntrega").value}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: '',
+    })
+
+    if (result.status == 200 || result.status == 201) {
+        alert("Endereço desabilitado com sucesso!");
+        window.location.reload();
+    } else {
+        alert("Falha ao desabilitar o endereço\nTente novamente!");
+    }
+});
+let showPassIcon = document.querySelector("#showPassword")
+showPassIcon.addEventListener("click", () => {
+    if (showPassIcon.getAttribute("class") == "fa-solid fa-eye-slash") {
+        showPassIcon.removeAttribute("class")
+        showPassIcon.setAttribute("class", "fa-solid fa-eye")
+        let passInput = document.querySelectorAll(".passInput")
+        for (let i = 0; i < passInput.length; i++) {
+            passInput[i].setAttribute("type", "text")
+        }
+
+    }
+    else {
+        showPassIcon.removeAttribute("class")
+        showPassIcon.setAttribute("class", "fa-solid fa-eye-slash")
+        let passInput = document.querySelectorAll(".passInput")
+        for (let i = 0; i < passInput.length; i++) {
+            passInput[i].setAttribute("type", "password")
+        }
+    }
+})
---
+++ target/classes/static/script/resumoPedido.js
@@ -0,0 +1,182 @@
+function setCookie(nome, info, exdays) {
+    Cookies.set(nome, info, exdays)
+}
+
+function getCookie(nome) {
+    return Cookies.get(nome)
+}
+
+function checkCookie(nome) {
+    var sessaoId = getCookie(nome);
+    if (sessaoId != undefined) {
+        document.getElementById("perfil").href = `./perfilCliente.html`;
+    }
+}
+checkCookie('sessaoId');
+
+var idCliente = getCookie('sessaoId');
+
+function menu() {
+    let nome_perfil = document.getElementById("nome-perfil");
+    let login_perfil = document.getElementById("login-perfil");
+    let sair = document.getElementById("sair");
+
+    if (idCliente == undefined) {
+        nome_perfil.innerHTML = "Seja Bem-Vindo(a)!";
+        login_perfil.innerHTML = "Login";
+        login_perfil.href = "./loginCliente.html";
+        sair.style.display = 'none';
+    } else {
+        nome_perfil.innerHTML = `Olá, ${getCookie("nome")}!`;
+        login_perfil.innerHTML = "Perfil";
+        login_perfil.href = "./perfilCliente.html";
+        sair.style.display = '';
+    }
+}
+menu();
+
+function desconectar() {
+    Cookies.remove('sessaoId');
+    Cookies.remove('nome');
+    sessionStorage.removeItem("carrinho");
+    window.location.reload();
+}
+let dados;
+const conectAPI = async () => {
+    dados = await fetch(`/cliente/${idCliente}`).then(data => data.json());
+    preecheDados();
+}
+
+conectAPI();
+
+const endEntrega = JSON.parse(sessionStorage.getItem("endEntrega"));
+
+const pagamento = sessionStorage.getItem('pagamento');
+
+var totalPago = parseFloat(endEntrega.valorFrete);
+
+async function carregarDados() {
+    const dadosPedido = await fetch(`/carrinho/search?cliente=${idCliente}`).then(response => response.json());
+
+    const tabela = document.getElementById("produtosTabela");
+
+    let quantidadeItens = 0;
+    dadosPedido.itens.forEach((item, index) => {
+
+
+        let tr = document.createElement("tr");
+        tr.setAttribute("class", "itemTabela");
+        tabela.appendChild(tr);
+
+        let prod = document.createElement("td");
+        prod.setAttribute("class", "itemProduto");
+        let imagem = document.createElement("td");
+        imagem.setAttribute("class", "imagemProduto");
+        let nomeProduto = document.createElement("td");
+        nomeProduto.setAttribute("class", "nomeProduto");
+        let quantidade = document.createElement("td");
+        quantidade.setAttribute("class", "quantidadeProduto")
+        let precoUnitario = document.createElement("td");
+        precoUnitario.setAttribute("class", "precoUnitarioProduto")
+        let precoTotal = document.createElement("td");
+        precoTotal.setAttribute("class", "precoTotalProduto");
+
+        let newLink;
+        if (item.produto.listaImagens.length != 0) {
+            item.produto.listaImagens.forEach(imagem => {
+                if (imagem.padrao) {
+                    let caminho = imagem.caminho;
+                    let link = caminho.split("/");
+                    newLink = "../" + link[5] + "/" + link[6] + "/" + link[7] + "/" + link[8];
+                }
+                if (imagem == null) {
+                    newLink = "../images/semImagem.webp"
+                }
+            });
+        } else {
+            newLink = "../images/semImagem.webp"
+        }
+
+        prod.textContent = ++quantidadeItens;
+        imagem.innerHTML = `<img src="${newLink}" style="width: 30px; height: 30px"></img>`;
+        nomeProduto.textContent = `${item.produto.nome}`;
+        quantidade.innerHTML = `${item.quantidade}`;
+        precoUnitario.textContent = `R$ ${parseFloat(item.produto.preco).toFixed(2).replace(".", ",")}`;
+        precoTotal.textContent = `R$ ${parseFloat(item.produto.preco * item.quantidade).toFixed(2).replace(".", ",")}`;
+
+        tr.appendChild(prod);
+        tr.appendChild(imagem)
+        tr.appendChild(nomeProduto);
+        tr.appendChild(quantidade);
+        tr.appendChild(precoUnitario);
+        tr.appendChild(precoTotal);
+
+        totalPago += (item.produto.preco * item.quantidade);
+        if (index == 0) {
+            let precoTotalPedido = document.createElement("td");
+            precoTotalPedido.setAttribute("class", "precoTotalPedido");
+            precoTotalPedido.textContent = `R$ ${parseFloat(totalPago).toFixed(2).replace(".", ",")}`;
+            tr.appendChild(precoTotalPedido)
+        }
+
+    });
+
+    let ultimaLinha = tabela.rows.length
+    console.log(tabela.rows.length);
+    let pedidoTotal = document.querySelector(".precoTotalPedido")
+    pedidoTotal.setAttribute("rowspan", ultimaLinha)
+    pedidoTotal.textContent = `R$ ${parseFloat(totalPago).toFixed(2).replace(".", ",")}`
+
+
+    document.getElementById("frete").textContent += `R$ ${parseFloat(endEntrega.valorFrete).toFixed(2).replace(".", ",")}`;
+
+    dados.listaEndereco.map(endereco => {
+        if (endereco.enderecoId == endEntrega.idEndEntrega) {
+            document.getElementById("cep").textContent += endereco.cep;
+            document.getElementById("logradouro").textContent += endereco.logradouro;
+            document.getElementById("numero").textContent += endereco.numero;
+            document.getElementById("complemento").textContent += endereco.complemento;
+            document.getElementById("bairro").textContent += endereco.bairro;
+            document.getElementById("cidade").textContent += endereco.cidade;
+            document.getElementById("uf").textContent += endereco.uf;
+        }
+    });
+
+    document.getElementById("pagamentoOpc").textContent = pagamento == "BO" ? "Boleto" : "Cartão de Crédito";
+}
+carregarDados()
+
+const preecheDados = async () => {
+
+    dados.listaEndereco.map(endereco => {
+        let opcao = document.createElement("option");
+        opcao.value = endereco.enderecoId;
+        if (endereco.enderecoId == endEntrega.idEndEntrega) {
+            document.getElementById("cep").value = endereco.cep;
+            document.getElementById("logradouro").value = endereco.logradouro;
+            document.getElementById("numero").value = endereco.numero;
+            document.getElementById("complemento").value = endereco.complemento;
+            document.getElementById("bairro").value = endereco.bairro;
+            document.getElementById("cidade").value = endereco.cidade;
+            document.getElementById("uf").value = endereco.uf;
+        }
+    });
+}
+
+async function finalizarPedido() {
+    const checkout = await fetch(`/pedido/import?cliente=${idCliente}&endereco=${endEntrega.idEndEntrega}&frete=${endEntrega.valorFrete}&modoPagamento=${pagamento}`, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: ""
+    });
+
+    if (checkout.status == 200 || checkout.status == 201) {
+        alert("Pedido realizado com sucesso!");
+        sessionStorage.removeItem("pagamento")
+        sessionStorage.removeItem("carrinho")
+        sessionStorage.removeItem("endEntrega")
+        window.open("../index.html", "_self");
+    } else {
+        alert("Erro ao realizar o pedido!");
+    }
+}
